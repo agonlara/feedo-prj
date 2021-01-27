246271ab88dbeafe1fae0252212440fd03e4e462
OTSuportTecnic <OTSuportTecnic@aoc.cat>
2021-01-27 19:44:40 +0100

    3 initial apps

---
+++ restaurant/src/main/java/ApplicationRestaurant.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationRestaurant {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
+}
